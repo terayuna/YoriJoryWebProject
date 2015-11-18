b12f9da2b0d727b4b94616ce068d560a23fb6d54
terayuna <terayuna@gmail.com>
2015-11-18 17:20:45 +0900

    mimi : YoriJoryWebProject 초기화

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -1,20 +1,68 @@
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.yorijory.webprj.dao.JdbcMemberDao;
 import com.yorijory.webprj.dao.MemberDao;
+import com.yorijory.webprj.dao.mybatis.MybatisMemberDao;
+import com.yorijory.webprj.dao.mybatis.SqlYojoSessionFactoryBuilder;
 import com.yorijory.webprj.vo.Member;
 
 public class TestProgram {
 
 	public static void main(String[] args) throws SQLException {
-		MemberDao dao = new MemberDao();
+		JdbcMemberDao dao = new JdbcMemberDao();
 		List<Member> list = dao.getMembers(2);
 		
+		Member upM = new Member();
+		upM.setMid("mid");
+		upM.setName("Å©Å©Å©");
+		upM.setPassword("333");
+		dao.update(upM);
+		
+		dao.delete("hayun");
+		
+/*		Member inM = new Member();
+		inM.setMid("hayun12");
+		inM.setName("Ä¼Ä¼");
+		inM.setPassword("1234");
+		dao.insert(inM);*/
+		
+		
+/********************MybatisÀÌ¿ë******************************	
+ 		
+ 		SqlSessionFactory ssf = SqlYojoSessionFactoryBuilder.getSqlSessionFactory(); //°øÀå¿¡¼­ ¼¼¼Ç°´Ã¼ ¾ò¾î¿È
+		SqlSession session = ssf.openSession();
+		MemberDao dao = session.getMapper(MemberDao.class); //ÀÎÅÍÆäÀÌ½º ±¸ÇöÇÑ Å¬·¡½º ¾ò¾î¿È
+ 		
+		MemberDao dao = new MybatisMemberDao();
+		List<Member> list = dao.getMembers(1, "MID", "");
+		
+		Member upM = new Member();
+		upM.setMid("GOD");
+		upM.setName("Å©Å©Å©");
+		upM.setPassword("333");
+		dao.update(upM);
+		
+		Member inM = new Member();
+		inM.setMid("hayun");
+		inM.setName("Ä¼Ä¼");
+		inM.setPassword("1234");
+		dao.insert(inM);
+		
+		dao.delete("GOD");
+	
+*****************************************************************************/
+		
+		System.out.println("°Ë»ö°á°ú : " + list.size());
 	
 		for(Member m : list){
 			System.out.printf("mid : %s, name : %s \n", m.getMid(), m.getName());
 		}
-
+		
+		
 	}
 
 }
---
+++ src/com/yorijory/webprj/dao/JdbcMemberDao.java
@@ -0,0 +1,126 @@
+package com.yorijory.webprj.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.PreparedStatement;
+import com.yorijory.webprj.vo.Member;
+
+public class JdbcMemberDao implements MemberDao{
+
+	public List<Member> getMembers() throws SQLException{
+		return getMembers(1,"MID", "");
+	}
+	
+	public List<Member> getMembers(int page) throws SQLException{
+		return getMembers(page, "MID", "");
+	}
+	
+	public List<Member> getMembers(int page, String field, String query) throws SQLException{
+	
+		
+		//String sql = "SELECT * FROM YJMember";
+		//String url = "jdbc:sqlserver://211.238.142.251:1433;databaseName=yorijorydb;";
+		//Connection con = DriverManager.getConnection(url, "yojo", "yjoriy1511");
+		
+		int start = 10*(page-1) + 1; 
+		int end = 10*page;
+		
+		String sql = "SELECT * FROM(SELECT ROW_NUMBER() OVER "
+				+ "(ORDER BY REGDATE DESC) NUM, * FROM MEMBERS WHERE " + field + " LIKE ?)A "
+				+ "WHERE NUM BETWEEN ? AND ?";
+		String url = "jdbc:sqlserver://211.238.142.251:1433;databaseName=yorijorydb;";
+		
+		
+		Connection con = DriverManager.
+				getConnection(url, "yojo", "yjoriy1511");
+		//Statement st = con.createStatement();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, "%"+query+"%");
+		st.setInt(2, start);
+		st.setInt(3, end);
+		ResultSet rs = st.executeQuery(); 
+		
+		
+		List<Member> list = new ArrayList<Member>();
+		Member member = null;
+		
+		
+		while(rs.next()) {
+			member = new Member();
+			
+			member.setMid(rs.getString("mid"));
+			member.setPassword(rs.getString("pwd"));
+			member.setName(rs.getString("name"));
+		
+			
+			list.add(member);
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
+		
+		
+		return list;
+	}
+
+	@Override
+	public int update(Member member) throws SQLException {
+		String sql = "UPDATE Members SET Name=?, Pwd=? WHERE Mid=?";
+		String url = "jdbc:sqlserver://211.238.142.251:1433;databaseName=yorijorydb;";
+		
+		Connection con = DriverManager.getConnection(url, "yojo", "yjoriy1511");
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, member.getName());
+		st.setString(2, member.getPassword());
+		st.setString(3, member.getMid());
+		
+		int count = st.executeUpdate();
+		
+		st.close();
+		con.close();
+
+		return count;
+	}
+
+	@Override
+	public int delete(String mid) throws SQLException {
+		String sql = "DELETE Members WHERE Mid=?";
+		String url = "jdbc:sqlserver://211.238.142.251:1433;databaseName=yorijorydb;";
+		
+		Connection con = DriverManager.getConnection(url, "yojo", "yjoriy1511");
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, mid);
+		
+		int count = st.executeUpdate();
+		
+		st.close();
+		con.close();
+
+		return count;
+	}
+
+	@Override
+	public int insert(Member member) throws SQLException {
+		String sql = "INSERT INTO Members(Mid, Name, Pwd) VALUES(?,?,?)";
+		String url = "jdbc:sqlserver://211.238.142.251:1433;databaseName=yorijorydb;";
+		
+		Connection con = DriverManager.getConnection(url, "yojo", "yjoriy1511");
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, member.getMid());
+		st.setString(2, member.getName());
+		st.setString(3, member.getPassword());
+
+		int count = st.executeUpdate();
+		
+		st.close();
+		con.close();
+
+		return count;
+	}
+}
