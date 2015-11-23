f1efa236942181ebddb17bf0dd88607ff0ebc543
sss <maggie8@naver.com>
2015-11-23 16:21:19 +0900

    sh - commentdao추가 테스트완료

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -4,64 +4,39 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import com.yorijory.webprj.dao.CommentDao;
 import com.yorijory.webprj.dao.JdbcMemberDao;
 import com.yorijory.webprj.dao.MemberDao;
+import com.yorijory.webprj.dao.mybatis.MybatisCommentDao;
 import com.yorijory.webprj.dao.mybatis.MybatisMemberDao;
 import com.yorijory.webprj.dao.mybatis.SqlYojoSessionFactoryBuilder;
+import com.yorijory.webprj.vo.Comment;
 import com.yorijory.webprj.vo.Member;
 
 public class TestProgram {
 
 	public static void main(String[] args) throws SQLException {
-		JdbcMemberDao dao = new JdbcMemberDao();
-		List<Member> list = dao.getMembers(2);
-		
-		Member upM = new Member();
-		upM.setMid("mid");
-		upM.setName("Å©Å©Å©");
-		upM.setPassword("333");
-		dao.update(upM);
-		
-		dao.delete("hayun");
-		
-/*		Member inM = new Member();
-		inM.setMid("hayun12");
-		inM.setName("Ä¼Ä¼");
-		inM.setPassword("1234");
-		dao.insert(inM);*/
 		
 		
-/********************MybatisÀÌ¿ë******************************	
- 		
- 		SqlSessionFactory ssf = SqlYojoSessionFactoryBuilder.getSqlSessionFactory(); //°øÀå¿¡¼­ ¼¼¼Ç°´Ã¼ ¾ò¾î¿È
-		SqlSession session = ssf.openSession();
-		MemberDao dao = session.getMapper(MemberDao.class); //ÀÎÅÍÆäÀÌ½º ±¸ÇöÇÑ Å¬·¡½º ¾ò¾î¿È
- 		
-		MemberDao dao = new MybatisMemberDao();
-		List<Member> list = dao.getMembers(1, "MID", "");
+		/*MemberDao dao = new MybatisMemberDao();
+		List<Member> list = dao.getMembers(1);
+		Member member = new Member();
 		
-		Member upM = new Member();
-		upM.setMid("GOD");
-		upM.setName("Å©Å©Å©");
-		upM.setPassword("333");
-		dao.update(upM);
 		
-		Member inM = new Member();
-		inM.setMid("hayun");
-		inM.setName("Ä¼Ä¼");
-		inM.setPassword("1234");
-		dao.insert(inM);
+		for(Member m : list)
+		{
+			System.out.printf("mid = %s, name = %s, %s\n", m.getMid(), m.getName(), m.getRegDate());
+		}*/
 		
-		dao.delete("GOD");
-	
-*****************************************************************************/
+		CommentDao dao = new MybatisCommentDao();
+		List<Comment> list = dao.getComments(1);
+		Comment comment = new Comment();
 		
-		System.out.println("°Ë»ö°á°ú : " + list.size());
-	
-		for(Member m : list){
-			System.out.printf("mid : %s, name : %s \n", m.getMid(), m.getName());
+		
+		for(Comment m : list)
+		{
+			System.out.printf("mid = %s, name = %s, %s\n", m.getMid(), m.getContent(), m.getRegDate());
 		}
-		
 		
 	}
 
