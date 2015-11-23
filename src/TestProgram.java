106ded659598f00d73fd5b78ba87a67d622d5cee
sss <maggie8@naver.com>
2015-11-23 16:44:54 +0900

    sh - messagedao 완료 테스트 ㅇㅋ

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -7,19 +7,30 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import com.yorijory.webprj.dao.CommentDao;
 import com.yorijory.webprj.dao.JdbcMemberDao;
 import com.yorijory.webprj.dao.MemberDao;
+import com.yorijory.webprj.dao.MessageDao;
 import com.yorijory.webprj.dao.mybatis.MybatisCommentDao;
 import com.yorijory.webprj.dao.NoticeDao;
 import com.yorijory.webprj.dao.mybatis.MybatisMemberDao;
+import com.yorijory.webprj.dao.mybatis.MybatisMessageDao;
 import com.yorijory.webprj.dao.mybatis.MybatisNoticeDao;
 import com.yorijory.webprj.dao.mybatis.SqlYojoSessionFactoryBuilder;
 import com.yorijory.webprj.vo.Comment;
 import com.yorijory.webprj.vo.Member;
+import com.yorijory.webprj.vo.Message;
 import com.yorijory.webprj.vo.Notice;
 
 public class TestProgram {
 
 	public static void main(String[] args) throws SQLException {
 	
+		MessageDao dao = new MybatisMessageDao();
+		List<Message> list = dao.getMessages(1);
+		Message message = new Message();
+		
+		
+		for(Message n : list)
+		{
+			System.out.printf("mid = %s, name = %s\n", n.getCode(), n.getTitle());
+		}
 	}
-
 }
