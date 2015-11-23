d1ae07cbf261714819c65ea590c7c7bef7c53e27
1 <june2126@naver.com>
2015-11-23 15:39:39 +0900

    AdminDao

---
+++ src/com/yorijory/webprj/dao/AdminDao.java
@@ -0,0 +1,19 @@
+package com.yorijory.webprj.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.yorijory.webprj.vo.Member;
+
+
+
+public interface AdminDao {
+	public List<Member> getMembers() throws SQLException;
+	public List<Member> getMembers(int page) throws SQLException;
+	public List<Member> getMembers(int page, String field, String query) throws SQLException;
+	public int update(Member member) throws SQLException; 
+	public int delete(String mid) throws SQLException; 
+	public int insert(Member member) throws SQLException; 
+
+
+}
